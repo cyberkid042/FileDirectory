be5b85a7e8ad8ef2f4a4040e9201f26aa117a6a3
cyberkid042 <cyberkid007@outlook.com>
2021-01-31 23:37:35 -0330

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Folder name="Desktop">
+        <Folder name="Music">
+          <Files name="Obinasom.mp3" />
+          <Files name="Celestial.mp3" />
+        </Folder>
+        <Files name="Assignment1.docx" />
+        <Files name="vacation.jpeg" />
+      </Folder>
+      <Folder name="Downloads" />
+      <Folder name="Documents" />
+      <Folder name="Applications" />
     </div>
   );
 }
+
+const Folder = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const arrowDirection = isOpen ? "down" : "right";
+
+  const openFolder = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <span onClick={openFolder}>
+        <i className="blue folder icon" style={{ cursor: "pointer" }}></i>
+        <i
+          className={`caret ${arrowDirection} icon`}
+          style={{ cursor: "pointer" }}
+        ></i>
+        {props.name}
+      </span>
+      <div style={{ marginLeft: "17px" }}>{isOpen ? props.children : null}</div>
+    </div>
+  );
+};
+
+const Files = ({ name }) => {
+  const fileExtension = name.split(".")[1];
+
+  const fileIcons = {
+    mp3: "headphones",
+    docx: "file word outline",
+    jpeg: "file image",
+  };
+
+  return (
+    <div>
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
+    </div>
+  );
+};
 
 export default App;
